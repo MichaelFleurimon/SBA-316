9e64d7916fd13ec62b08983d0e4bab6c392a5be3
Moose <Smithblack7854@gmail.com>
2024-12-12 18:50:53 -0500

    this commit pushes the correct files, my github was very mixed up and I was pushing the wrong files

---
+++ sba-316.js
@@ -0,0 +1,24 @@
+document.title = "QuizMaker";
+// obj that holds a array of objects
+const quizQuestion = [{
+    question:"what kind of matieral do you like?",
+    answers:["metal","wood","concreat/stone","plastic"]
+},
+{
+question:"what kind of paintings do you like?",
+answers:["landscapes","abstract","historical","modern"]
+},
+{
+    question:"what colors would you like your bedroom to be?",
+    answers:["cool colors","warm colors"]
+}
+];
+const startbnt= document.getElementById("start-btn");
+const questionList = document.getElementById("")
+console.log("I work");
+function startQuiz(){
+
+}
+function addQuestion(){
+
+}
